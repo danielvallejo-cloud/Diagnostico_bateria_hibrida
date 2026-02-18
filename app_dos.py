f94ff4e02b30ceb2ff543cc50561bb210b5008e4
danielvallejo-cloud <daniel.vallejo@unl.edu.ec>
2026-02-18 18:11:33 -0500

    Add files via upload

--- app_dos.py
+++ app_dos.py
@@ -6,7 +6,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 
 # ============================================================
 # -------------------- MODULO ANALITICO ----------------------
